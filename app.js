d1e31150f390614ac5d639782ba32ebc7febc616
Ryan Hirte <rhirte@ewu.edu>
2024-07-24 19:57:03 -0700

    added api key and urls

---
+++ app.js
@@ -0,0 +1,4 @@
+const API_KEY = "b67bfd5ea3cb0a7ca5128826cb92cb7d";
+
+// api weather url: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// api geocoding url: http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid={API key}
